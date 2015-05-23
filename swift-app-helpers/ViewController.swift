53f4631595d91d54f0b5c2c725f1f488c9c443d1
bensoer <ikben@shaw.ca>
2015-05-23 15:57:27 -0700

    Initial Commit

---
+++ swift-app-helpers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-app-helpers
+//
+//  Created by Ben Soer on 2015-05-23.
+//  Copyright (c) 2015 bensoer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
